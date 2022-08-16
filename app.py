5904373fcd456b83fb70f1ecf33216b430cff799
cybermin <sillamin@naver.com>
2022-08-16 14:41:07 +0900

    app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+import streamlit as st
+import pandas as pd
+import numpy as np
+import json
 
-cred = credentials.Certificate("app2022-a58d3-firebase-adminsdk-cstfx-ecff887237.json")
-firebase_admin.initialize_app(cred)
 
-dir = db.reference()
-dir.update({'자동차':['기아','현대','벤츠']}) 
+with open('onoff.json', 'r') as fp:
+    data = json.loads(fp)
+
+
+st.write(data)
